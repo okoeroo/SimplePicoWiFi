ffa575e1157515bb7b11bff79317131a700db40e
Oscar Koeroo <okoeroo@gmail.com>
2023-01-05 22:43:56 +0100

    new connect

--- SimplePicoWiFi.py
+++ SimplePicoWiFi.py
@@ -1,30 +1,39 @@
 import network
-import utime as time
+from machine import Pin
+import utime
+
 
 
 class SimplePicoWiFi():
     # Wait for connect success or failure
     # Max wait tries
-    max_wait = 25
-
+    max_wait = 30
+    
     def __init__(self, ssid, wifi_psk):
         # Check if wifi details have been set
         if len(ssid) == 0 or len(wifi_psk) == 0:
             raise RuntimeError('Please set wifi ssid and password in this script')
-
+        
         self._ssid = ssid
         self._psk = wifi_psk
-
+        
         self.wlan = None
+        
+        self.led = Pin("LED", Pin.OUT)
+        self.led.off()
 
     def disconnect(self):
         self.wlan.active(False)
-
+    
     def connect(self):
+        self.led.off()
+        
         # Start connection
         self.wlan = network.WLAN(network.STA_IF)
         self.wlan.active(True)
         self.wlan.connect(self._ssid, self._psk)
+
+        self.led.toggle()
 
         while self.max_wait > 0:
             # Check for failure or connected
@@ -32,13 +41,21 @@ class SimplePicoWiFi():
                 break
             self.max_wait -= 1
             print('waiting for connection...', self.wlan.status(), self.get_status())
-            time.sleep_ms(400)
+            utime.sleep_ms(400)
+            
+            self.led.toggle()
 
         # Handle connection error
         if self.wlan.status() != 3:
+            x = range(10)
+            for n in x:
+                self.led.toggle()
+                utime.sleep_ms(200)
             raise RuntimeError('wifi connection failed %d' % self.wlan.status())
 
-        print('status:', self.get_status())
+        # High
+        self.led.high()
+        return True
 
     def get_status(self):
         """
@@ -67,22 +84,22 @@ class SimplePicoWiFi():
             return 'badauth'
         else:
             return 'unknown status'
-
+    
     def get_ssid(self):
         return self._ssid
-
+    
     def get_ip(self):
         if self.wlan.status() != 3:
             return None
         else:
             return self.wlan.ifconfig()[0]
-
+                
     def get_subnet(self):
         if self.wlan.status() != 3:
             return None
 
         return self.wlan.ifconfig()[1]
-
+    
     def get_gateway(self):
         if self.wlan.status() != 3:
             return None
@@ -94,7 +111,7 @@ class SimplePicoWiFi():
             return None
 
         return self.wlan.ifconfig()[3]
-
+    
     def get_info_json(self) -> str:
         return "\n".join([ "\"wifi\": {",
                           f"    \"ssid\": \"{self.get_ssid()}\"",
@@ -104,36 +121,33 @@ class SimplePicoWiFi():
                           f"    \"gateway\": \"{self.get_gateway()}\"",
                           f"    \"dns\": \"{self.get_dns()}\"",
                            "}"])
-
-    def get_info(self) -> str:
+        
+    def get_info(self) -> str:        
         return ",".join([f"ssid:\"{self.get_ssid()}\"",
                          f"status:{self.get_status()}",
                          f"ip:{self.get_ip()}",
                          f"subnet:{self.get_subnet()}",
                          f"gateway:{self.get_gateway()}",
                          f"dns:{self.get_dns()}"])
-
+    
     def __str__(self) -> str:
         return self.get_info()
-
+    
     def __repr__(self) -> str:
         return f"SimplePicoWiFi({self.get_info()})"
-
+        
 
 
 if __name__ == "__main__":
     import urequests as requests
-    import time
-
-    from wifi import SimplePicoWiFi
 
 
     WIFI_SSID = const('example SSID')
     WIFI_PASSWORD = const('example WiFi password')
-
+        
     wifi = SimplePicoWiFi(WIFI_SSID, WIFI_PASSWORD)
     wifi.connect()
-
+    
     print(wifi.get_info())
     print(wifi)
     print(wifi.__repr__())
